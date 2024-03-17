bd256d544cae3154df2cf504c70c79ee22e0e473
jonathanphill <jonathanphill@gmail.com>
2024-03-17 13:35:39 -0400

    finishing the app with tailwind css

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -23,11 +23,28 @@ import {styled} from 'styled-components';
 //   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 // `;
 
-export default function CustomInput({ label, invalid, ...props }) {
+export default function Input({ label, invalid, ...props }) {
+    let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase "
+    let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow  "
+
+    if(invalid){
+        labelClasses += "text-red-400";
+        inputClasses += "text-red-500 bg-red-100 border-red-300";
+    }else{
+        labelClasses += "text-stone-300";
+        inputClasses += "text-gray-700 bg-stone-300";
+    }
+
+
   return (
     <p>
-      <Label $invalid={invalid}>{label}</Label>
-      <Input $invalid={invalid} {...props} />
+      <label className={labelClasses}>
+        {label}
+      </label>
+      <input
+        className={inputClasses}
+        {...props}
+      />
     </p>
   );
 }
